afb26c7c50f6cfa7b2202e20356979010af906fa
Ray Sun <xiaoquqi@gmail.com>
2015-03-31 08:06:29 +0800

    Optimize vim_init.sh
    Comment set directory in vimrc

--- vim_init.sh
+++ vim_init.sh
@@ -3,13 +3,12 @@
 # This scirpt used for vim configuration initialization
 # Author: Ray Sun
 # Email: xiaoquqi@gmail.com
-# Latest Update: 2012-07-14
+# Latest Update: 2015-03-30
 
-# Make link to .vimrc and .vim directory, if already existing, mv them to
-# .vimrc.old.timestamp .vim.old.timestamp
+# Make link to .vimrc and .vim directory, if they are already existing,
+# rename them to .vimrc.bak .vim.bak
 
-cd $(dirname $0)
-BASE_DIR=`pwd`
+BASE_DIR=$(cd $(dirname $0); pwd)
 
 NEW_VIMRC="$BASE_DIR/vim/vimrc"
 NEW_VIM="$BASE_DIR/vim"
@@ -17,56 +16,23 @@ NEW_VIM="$BASE_DIR/vim"
 DOT_VIMRC="$HOME/.vimrc"
 DOT_VIM="$HOME/.vim"
 
-TIMESTAMP=`date +"%y%m%d%H%m%S"`
-DOT_VIMRC_BAK="$DOT_VIMRC.$TIMESTAMP"
-DOT_VIM_BAK="$DOT_VIM.$TIMESTAMP"
+DOT_VIMRC_BAK="$DOT_VIMRC.bak"
+DOT_VIM_BAK="$DOT_VIM.bak"
 
-# return true if the same file
-function same_file {
-  link_source_file=`readlink $1`
-  if [ $link_source_file -ef $2 ]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
-# backup file and create link
-function create_link {
-  echo "Your $1 backup to $2"
-  mv $1 $2
-  ln -s $3 $1
-}
-
-# .vimrc
-if [ -e $DOT_VIMRC ]; then
-  # if it's already a link, test if it's link to us
-  same_file $DOT_VIMRC $NEW_VIMRC
-  is_same_file=$?
-  if [ -L $DOT_VIMRC ] && [ $is_same_file -eq 1 ]
-  then
-    echo "Link existing no need to create for $DOT_VIMRC"
-  else
-    create_link $DOT_VIMRC $DOT_VIMRC_BAK $NEW_VIMRC
-  fi
-  else
-    create_link $DOT_VIMRC $DOT_VIMRC_BAK $NEW_VIMRC
+if [[ -e $DOT_VIMRC ]]; then
+  echo "Backing up $DOT_VIMRC to $DOT_VIMRC_BAK"
+  mv $DOT_VIMRC $DOT_VIMRC_BAK
 fi
+ln -s $NEW_VIMRC $DOT_VIMRC
 
-# .vim
-if [ -e $DOT_VIM ]; then
-  # if it's already a link, test if it's link to us
-  same_file $DOT_VIM $NEW_VIM
-  is_same_file=$?
-  if [ -L $DOT_VIM ] && [ $is_same_file -eq 1 ]
-  then
-    echo "Link existing no need to create for $DOT_VIM"
-  else
-    create_link $DOT_VIM $DOT_VIM_BAK $NEW_VIM
+if [[ -e $DOT_VIM ]]; then
+  echo "Backing up $DOT_VIM to $DOT_VIM_BAK"
+  if [[ -e $DOT_VIM_BAK ]]; then
+    rm -rf $DOT_VIM_BAK
   fi
-  else
-    create_link $DOT_VIM $DOT_VIM_BAK $NEW_VIM
+  mv $DOT_VIM $DOT_VIM_BAK
 fi
+ln -s $NEW_VIM $DOT_VIM
 
 # Go back to dotfiles do git submodule init
 cd $BASE_DIR
@@ -75,4 +41,4 @@ git submodule update --init
 # Install vim plugins
 vim +BundleInstall +qall
 
-echo "Configurate VIM Done"
+echo "Vim Configuration Done"
